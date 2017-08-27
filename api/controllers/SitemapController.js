41c67c4cd2bfb662049450fb5feb29c98f7f064e
Jose Luis Raffalli <raffallijoseluis@gmail.com>
2017-08-27 18:32:03 -0400

    SITEMAP

---
+++ api/controllers/SitemapController.js
@@ -0,0 +1,30 @@
+/**
+ * SitemapController
+ *
+ * @description :: Server-side logic for managing Sitemaps
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	build_sitemap: function(req, res) {
+        var fs = require('fs');
+        
+        var SitemapGenerator = require('sitemap-generator');
+        var generator = new SitemapGenerator('http://raffallijoseluis.com.ve');
+        
+        generator.on('done', function (sitemap) {
+            console.log(sitemap);
+ 
+            fs.writeFile("./assets/sitemap.xml", sitemap, function(err){
+                if(err) {
+                    return res.negotiate(err);
+                }
+                console.log("The file was saved!");
+                return;  
+            });
+        });
+        
+        generator.start();
+    },
+};
+
