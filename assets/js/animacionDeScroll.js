6aaa32ca36e7686cbdbeb49ee22dc4764fc1c0b2
Jose Luis Raffalli <raffallijoseluis@gmail.com>
2017-09-05 21:45:21 -0400

    WTF

--- assets/js/animacionDeScroll.js
+++ assets/js/animacionDeScroll.js
@@ -1,4 +1,15 @@
-
+$(function() {
+    $('.chart-easy').easyPieChart({
+      scaleColor: "#ecf0f1",
+      lineWidth: 20,
+      lineCap: 'butt',
+      barColor: '#1abc9c',
+      trackColor:	"#ecf0f1",
+      delay: 5000,
+      size: 160,
+      animate: 4000
+    });
+  });
 var EPPZScrollTo =
 {
     /**
